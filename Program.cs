d3927a968d2a336998e000943f42f52237881189
Marczinko Istvan <marczinko010509@gmail.com>
2020-09-21 14:22:23 +0200

    min és max meghatározás

--- Program.cs
+++ Program.cs
@@ -29,6 +29,22 @@ namespace VerzioKezelesHF
             }
             atlag = atlag / darabszam;
 
+            int min = bekert[0];
+            int max = bekert[0];
+
+            for (int i = 0; i < bekert.Length; i++)
+            {
+                if (min > bekert[i])
+                {
+                    min = bekert[i];
+                }
+                if (max < bekert[i] )
+                {
+                    max = bekert[i];
+                }
+            }
+
+            Console.WriteLine("A min érték: " + min + ", a max érték: " + max);
         }
     }
 }
