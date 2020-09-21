0b400087811697c57095ccf33133475a0587403b
Marczinko Istvan <marczinko010509@gmail.com>
2020-09-21 14:18:08 +0200

    10 szám bekérés, átlag kiírás

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            beker();
             Console.ReadKey();
+        }
+
+        public static void beker() {
+            Console.WriteLine("Kérem adjon meg 10 számot!");
+            int darabszam = 10;
+            int[] bekert = new int[darabszam];
+            for (int i = 0; i <  bekert.Length; i++)
+            {
+                bekert[i] = int.Parse(Console.ReadLine());
+            }
+            int atlag = 0;
+            for (int i = 0; i < bekert.Length; i++)
+            {
+                atlag = atlag + bekert[i];
+            }
+            atlag = atlag / darabszam;
+
         }
     }
 }
